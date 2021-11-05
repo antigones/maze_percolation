3a94c6fcf60adda4a31da928716cab09abf5062d
Juna Salviati <antigones@users.noreply.github.com>
2021-11-05 17:31:26 +0100

    Delete test_site_percolation.py

--- test_site_percolation.py
+++
@@ -1,9 +0,0 @@
-import maze_site_percolation
-
-def main():
-    pm = maze_site_percolation.MazeSitePercolationModel(p=0.4, size=50)
-    pm.pretty_output_grid_image('maze_site_percolation.gif', add_walls=True)
-
-if __name__ == '__main__':
-    main()
-    
